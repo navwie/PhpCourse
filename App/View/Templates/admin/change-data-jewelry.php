0c92ec1a8e401c4c1cd9e31d32f7e7d56c4ef135
alesia <navwie@gmail.com>
2022-01-15 20:39:54 +0200

    Add admin user to site

---
+++ App/View/Templates/admin/change-data-jewelry.php
@@ -0,0 +1,32 @@
+<div class="page">
+    <div class="page-sign-up">
+        <div class="d-flex col-12 form-signin container justify-content-center">
+            <form action="changeJewelry" method="post">
+                <h1 class="h3  fw-normal text-center">Обновить</h1>
+                <div class="form-group mt-4">
+                    <label for="title">Название</label>
+                    <input type="number" id="title" name="title">
+                </div>
+                <div class="form-group mt-4">
+                    <label for="amount">Количество</label>
+                    <input type="number" id="amount" name="amount" >
+                </div>
+                <div class="form-group mt-4">
+                    <label for="description">Описание</label>
+                    <input type="text" id="description" name="description" >
+                </div>
+                <div class="form-group mt-4">
+                    <label for="price">Цена</label>
+                    <input type="number" id="price" name="price">
+                </div>
+                <div class="form-group mt-4">
+                    <label for="image">Фото</label>
+                    <input type="file" id="image" name="image" >
+                </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-outline-dark">Изменить</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
--- Framework/Templeater/Templeater.php
+++ Framework/Templeater/Templeater.php
@@ -11,11 +11,20 @@ class Templeater
         $content = ob_get_clean();
         require_once '../App/View/Layouts/mainLayout.php';
     }
-    public function renderUser($title, $view, $vars = [])
+
+    public function renderUser($title, $view, $params = [])
     {
         ob_start();
         require_once "../App/View/Templates/user/"  . $view . ".php";
         $content = ob_get_clean();
-        require_once '../App/View/Layouts/mainLayout.php';
+        require_once '../App/View/Layouts/userLayout.php';
+    }
+
+    public function renderAdmin($title, $view, $params = [])
+    {
+        ob_start();
+        require_once "../App/View/Templates/admin/"  . $view . ".php";
+        $content = ob_get_clean();
+        require_once '../App/View/Layouts/adminLayout.php';
     }
 }
